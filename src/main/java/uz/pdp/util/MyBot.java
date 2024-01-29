36ffdd29ae27f40e20b728552c449a650504bd36
Samandar <asyncdeveloper023@gmail.com>
2024-01-29 16:20:47 +0500

    Sending massage is starting

--- src/main/java/uz/pdp/util/MyBot.java
+++ src/main/java/uz/pdp/util/MyBot.java
@@ -11,6 +11,7 @@ import uz.pdp.entity.TelegramUser;
 import uz.pdp.enums.Status;
 import uz.pdp.service.TelegramUserRepo;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -19,7 +20,7 @@ public class MyBot {
 
     private  String token =  "6871863879:AAGFyjh3JK5mJcFgCN0w_4Wa_KgKl7zHcTQ";
     public  TelegramBot telegramBot = new TelegramBot(token);
-    //public static TelegramUserRepo userRepo = TelegramUserRepo.getInstance();
+    public static TelegramUserRepo userRepo = TelegramUserRepo.getInstance();
 
 
     public void start() {
@@ -48,7 +49,15 @@ public class MyBot {
             Contact contact = update.message().contact();
             String phonedNumber = contact.phoneNumber();
             currentUser.setPhoneNumber(phonedNumber);
-            currentUser.setStatus(Status.SHARECONTACT);
+            System.out.println(currentUser.getStatus().equals(Status.SHARECONTACT));
+            if (currentUser.getStatus().equals(Status.SHARECONTACT)) {
+                String langText = getLangText(currentUser.getChatLang(),"SEND_MESSAGE");
+                sendMassageInBotToUsers(userRepo.getAll(),id,langText);
+                currentUser.setStatus(Status.SEND_MESSAGE);
+
+            }
+
+
         }
 
         else {
@@ -67,6 +76,8 @@ public class MyBot {
                 currentUser.setStatus(Status.LANGUAGE);
             } else {
 
+                System.out.println(currentUser.getStatus());
+
                 if (currentUser.getStatus().equals(Status.LANGUAGE)) {
 
                     String lang = update.message().text();
@@ -83,18 +94,35 @@ public class MyBot {
                     String shareContact = getLangText(currentUser.getChatLang(), "SHARE_CONTACT");
                     sendMassageToBot(shareContact, id,currentUser);
                     currentUser.setStatus(Status.SHARECONTACT);
+                    System.out.println(currentUser.getStatus());
 
-                } else if (currentUser.getStatus().equals(Status.SHARECONTACT)) {
+                } else if (currentUser.getStatus().equals(Status.SEND_MESSAGE)) {
 
-
+                    System.out.println("Salom");
 
                 }
+            }
+        }
+    }
 
-
+    private void sendMassageInBotToUsers(List<TelegramUser> users, Long id, String sendText ) {
+        int i=0;
+        String[] strings = new String[users.size()-1];
+        for (TelegramUser user : users) {
+            if(!user.getChatId().equals(id)){
+                strings[i++] = user.getName();
             }
 
         }
 
+        System.out.println(Arrays.deepToString(strings));
+        System.out.println("Salom");
+
+        KeyboardButton keyboardButton = new KeyboardButton(sendText);
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(strings);
+        SendMessage message = new SendMessage(id,sendText);
+        message.replyMarkup(replyKeyboardMarkup);
+        telegramBot.execute(message);
 
     }
 
@@ -127,11 +155,8 @@ public class MyBot {
         telegramBot.execute(sendMessage);
     }
 
-
-
     private String getLangText(String chatLang, String textLang) {
 
-        //Locale locale = Locale.of(chatLang);
         Locale locale = new Locale(chatLang);
         ResourceBundle bundle = ResourceBundle.getBundle("message", locale);
 
@@ -153,7 +178,7 @@ public class MyBot {
     }
 
     private TelegramUser getUser(Long id) {
-        TelegramUserRepo userRepo = TelegramUserRepo.getInstance();
+
         List<TelegramUser> users = userRepo.getAll();
         for (TelegramUser user : users) {
             if(user.getChatId().equals(id)){
